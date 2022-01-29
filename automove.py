155cd5a6415c6d0c7fbf2952bfbb6359a3e0452b
demirezenmert <mertdemirezen@Merts-MacBook-Pro.local>
2022-01-29 00:16:50 -0500

    Tag of file Number feauter added

--- automove.py
+++ automove.py
@@ -4,14 +4,21 @@ from watchdog.events import FileSystemEventHandler
 import os,json
 
 class MyHandler(FileSystemEventHandler):
-    def __init__(self,folderCounter = 1):
-        self.folderCounter = folderCounter
+    def __init__(self,fileCounter = 1):
+        self.fileCounter = fileCounter
     def on_modified(self, event):
-        # for filename in os.listdir(folder_to_track):
-        #     src = folder_to_track + '/'+ filename
-        #     newDestination = folderDestination + '/'+ filename
-        #     os.rename(src, newDestination)
-        newName = str(self.i)
+        
+        for filename in os.listdir(folder_to_track):
+            newName = str(self.fileCounter) + "_newFile" + filename
+            fileExists = os.path.isfile(folderDestination+ '/'+ newName)
+            while fileExists:
+                self.fileCounter += 1
+                newName = str(self.fileCounter) + "_newFile_" + filename
+            self.fileCounter += 1
+            src = folder_to_track + '/'+ filename
+            newDestination = folderDestination + '/'+ newName
+            os.rename(src, newDestination)
+        
 
 folder_to_track = 'firstFolder'
 folderDestination = 'secondFolder'
